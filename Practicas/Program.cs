7cdfec2a1b15415557c57cb3fbc318c2e5ed0eab
hcontrerastorrez <hcontreras.torrez@gmail.com>
2024-04-26 13:32:07 -0400

    Solucion Kata FirstNonRepeatingLetter

--- Practicas/Program.cs
+++ Practicas/Program.cs
@@ -1,11 +1,8 @@
-﻿using codewars;
-using Practicas.codewars;
+﻿using Practicas.codewars;
 namespace Practices {
     public class Program{
-       static KataNextBiggerNumber test = new KataNextBiggerNumber();
         public static void Main(string[] args){
-            int n = 225;
-            Console.WriteLine(KataRgbToHex.RgbKata(225,0,330));
+            Console.WriteLine(KataFirstNonRepeatingLettercs.FirstNonRepeatingLetter("Camel Case My Little Friend"));
         }
     }
 }
---
+++ Practicas/codewars/KataFirstNonRepeatingLettercs.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practicas.codewars
+{
+    public class KataFirstNonRepeatingLettercs
+    {
+        public static string FirstNonRepeatingLetter(string s) {
+            string fnrp = string.Empty; 
+            foreach (var c in s.ToCharArray())
+            {
+                if (s.ToLower().Split(c.ToString().ToLower()).Length > 2) fnrp = "";
+                else return c.ToString();  
+            }
+            return fnrp;
+        }
+    }
+}
